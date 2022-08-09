5a0edd90147cb4e60148394868e554e146181937
HBiridepalli <hbiridepalli@apptium.com>
2022-08-09 09:48:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tlr8jq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tlr8jq)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, MatCardModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [],
+})
+export class AppModule {}
